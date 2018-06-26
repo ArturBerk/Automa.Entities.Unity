f1b289f09aa2c85496d4e4da0c2b0972c46e41f7
ArturBerk <berkmail@mail.ru>
2018-06-26 22:55:07 +0300

    ComponentTemaplte with builder

--- Templates/ComponentTemplate.cs
+++ Templates/ComponentTemplate.cs
@@ -1,4 +1,5 @@
-﻿using Automa.Entities.Builders;
+﻿using System.Collections.Generic;
+using Automa.Entities.Builders;
 using UnityEngine;
 
 namespace Automa.Entities.Unity.Templates
@@ -10,12 +11,25 @@ namespace Automa.Entities.Unity.Templates
 
     public class ComponentTemplate<TParameter, TComponent> : ComponentTemplate<TParameter>
     {
-        [SerializeField]
-        private TComponent component;
+        [SerializeField] private TComponent component;
 
         public override IComponentBuilder<TParameter> GetBuilder()
         {
             return new ComponentValue<TParameter, TComponent>(component);
+        }
+    }
+
+    public abstract class ComponentTemplateWithBuilder<TParameter> : ComponentTemplate<TParameter>,
+        IComponentBuilder<TParameter>
+    {
+        public abstract void Build(ref EntityReference entity, ref TParameter parameter,
+            Dictionary<string, EntityReference> templateEntities);
+
+        public abstract ComponentType[] Types { get; }
+
+        public override IComponentBuilder<TParameter> GetBuilder()
+        {
+            return this;
         }
     }
 }
